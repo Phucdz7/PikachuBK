3c1d64ae5da1c9e1989f8983fad3b0c418990e46
Phucdz7 <luongphuc102002@gmail.com>
2025-01-24 11:25:21 +0700

    Initialize

---
+++ Assets/_Data/RotateObject.cs
@@ -0,0 +1,18 @@
+
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    void Start()
+    {
+      //  Camera.main.transform.rotation = Quaternion.Euler(0, 0, 90);
+
+        /*
+        foreach (GameObject obj in GameObject.FindGameObjectsWithTag("Default"))
+        {
+            obj.transform.rotation = Quaternion.Euler(0, 0, 90);
+        }
+        */
+    }
+
+}
